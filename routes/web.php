ae5aa7cc23bf8bfc6d8ed34ce67c634955ad9a92
Liushouping <kose146352@gmail.com>
2020-11-30 14:47:16 +0800

    生成用戶憑證代碼

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
