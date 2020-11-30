b6a0527098c3bba9ca8965ee57c9963a54ab91d4
Liushouping <kose146352@gmail.com>
2020-11-30 16:33:17 +0800

    分類話題

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -9,10 +9,14 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <!-- Left Side Of Navbar -->
-    <ul class="navbar-nav mr-auto">
-
-    </ul>
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item {{ active_class(if_route('topics.index')) }}"><a class="nav-link" href="{{ route('topics.index') }}">话题</a></li>
+        <li class="nav-item {{ category_nav_active(1) }}"><a class="nav-link" href="{{ route('categories.show', 1) }}">分享</a></li>
+        <li class="nav-item {{ category_nav_active(2) }}"><a class="nav-link" href="{{ route('categories.show', 2) }}">教程</a></li>
+        <li class="nav-item {{ category_nav_active(3) }}"><a class="nav-link" href="{{ route('categories.show', 3) }}">问答</a></li>
+        <li class="nav-item {{ category_nav_active(4) }}"><a class="nav-link" href="{{ route('categories.show', 4) }}">公告</a></li>
+      </ul>
 
     <!-- Right Side Of Navbar -->
     <ul class="navbar-nav navbar-right">
@@ -20,6 +24,7 @@
         @guest
         <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        
         @else
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
